7066e06cd18770bdad5eca791de5a12eae23545e
omdubey <omdubey1021@gmail.com>
2023-08-17 14:55:52 +0530

    changed jwt

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -2,19 +2,14 @@ import createError from "../utils/createError.js";
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  
   const token = req.cookies.accessToken;
   if (!token) return next(createError(404, "You are not authenticated"));
-  
+
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) return next(createError(403, "Token is not valid"));
     req.userId = payload.id;
     req.isSeller = payload.isSeller;
-    res.cookie('accessToken', token, {
-      secure: true,
-      sameSite: 'None',
-      // other cookie options...
-  });
+
     next();
   });
 };
