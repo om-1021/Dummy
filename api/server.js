66b9b76df2a85bcfa5ac1ac3fa4741f64c8131c4
omdubey <omdubey1021@gmail.com>
2023-08-27 12:39:42 +0530

    fixed cors policy-2

--- api/server.js
+++ api/server.js
@@ -173,7 +173,7 @@ const connect = async () => {
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://64eaf407067a023bd526a617--spiffy-kleicha-742e77.netlify.app"
+    "https://64eaf657edd7a73719f59a66--melodious-kitsune-b305fb.netlify.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
@@ -187,7 +187,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin:
-      "https://64eaf407067a023bd526a617--spiffy-kleicha-742e77.netlify.app",
+      "https://64eaf657edd7a73719f59a66--melodious-kitsune-b305fb.netlify.app",
     credentials: true,
   })
 );
--- client/src/helper.js
+++ client/src/helper.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://gigster-backend-algokings.onrender.com";
+export const BASE_URL = "https://gigster-backend-algokings.onrender.com/api";
