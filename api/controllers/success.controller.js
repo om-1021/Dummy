57e4aaeeb6776d0ce94bd2fd05218f92db3923e7
omdubey <omdubey1021@gmail.com>
2023-08-27 21:33:37 +0530

    fix: route for success page x9

---
+++ api/controllers/success.controller.js
@@ -0,0 +1,29 @@
+import express, { json } from "express";
+// import router from './user.route';
+// const stripe = require('stripe')(process.env.STRIPE);
+import Stripe from "stripe";
+// import {stripe} from {stripe}
+// const app = express();
+const router = express.Router();
+
+// app.use(json());
+const stripe = new Stripe(process.env.STRIPE);
+
+export const successController = async (req, res) => {
+  const { items } = req.body; // Items you want to include in the checkout
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: items,
+      success_url: "https://yourwebsite.com/success",
+      cancel_url: "https://yourwebsite.com/cancel",
+    });
+    console.log(res);
+    res.json({ sessionId: session.id });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+
--- api/routes/succes.route.js
+++ api/routes/succes.route.js
@@ -1,29 +1,8 @@
-import express, { json } from "express";
-// import router from './user.route';
-// const stripe = require('stripe')(process.env.STRIPE);
-import Stripe from "stripe";
-// import {stripe} from {stripe}
-// const app = express();
+import express from "express";
+import { successController } from "../controllers/success.controller.js";
+
 const router = express.Router();
 
-// app.use(json());
-const stripe = new Stripe(process.env.STRIPE);
-
-router.post("/", async (req, res) => {
-  const { items } = req.body; // Items you want to include in the checkout
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: items,
-      success_url: "https://yourwebsite.com/success",
-      cancel_url: "https://yourwebsite.com/cancel",
-    });
-    console.log(res);
-    res.json({ sessionId: session.id });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+router.get("/", successController);
 
 export default router;
