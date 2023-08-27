0eb7298768327578cae558f760b59a48bc191e65
omdubey <omdubey1021@gmail.com>
2023-08-27 20:21:59 +0530

    fix: route for success page x3

--- api/routes/succes.route.js
+++ api/routes/succes.route.js
@@ -1,11 +1,12 @@
 import express, { json } from 'express';
-import router from './user.route';
+// import router from './user.route';
 const stripe = require('stripe')('YOUR_SECRET_KEY');
-const app = express();
+// const app = express();
+const router = express.Router();
 
-app.use(json());
+// app.use(json());
 
-app.post('/create-checkout-session', async (req, res) => {
+router.post('/create-checkout-session', async (req, res) => {
   const { items } = req.body; // Items you want to include in the checkout
   try {
     const session = await stripe.checkout.sessions.create({
